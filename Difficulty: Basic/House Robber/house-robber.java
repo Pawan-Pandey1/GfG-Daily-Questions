e84a2a55ed585a84a7fb53fc865735151729f7eb
Pawan Pandey <pawanpandey.3002@gmail.com>
2025-07-22 12:10:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/House Robber/house-robber.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+
+class Solution {
+    static int maximizeMoney(int N, int K) {
+        // code here
+        int x=(N+1)/2;
+        
+        return x*K;
+    }
+};
