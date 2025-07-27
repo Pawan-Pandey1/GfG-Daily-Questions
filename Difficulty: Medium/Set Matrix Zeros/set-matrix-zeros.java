ade32e40537c60728f13de68a233088b7d2f8cbc
Pawan Pandey <pawanpandey.3002@gmail.com>
2025-07-27 20:00:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Set Matrix Zeros/set-matrix-zeros.java
+++ Difficulty: Medium/Set Matrix Zeros/set-matrix-zeros.java
@@ -1,6 +1,5 @@
 class Solution {
-    private int MARKER = Integer.MIN_VALUE + 1;
-    
+    private int marker=Integer.MIN_VALUE+1;
     public void setMatrixZeroes(int[][] mat) {
         // code here
         int row=mat.length;
@@ -10,30 +9,30 @@ class Solution {
             for(int j=0;j<col;j++){
                 if(mat[i][j]==0){
                     setRow0(i,mat);
-                    setCol0(j,mat);;
+                    setCol0(j,mat);
                 }
             }
         }
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(mat[i][j]==MARKER){
-                    mat[i][j]=0;
-                }
-            }
-        }
-    }
-    private void setRow0(int r,int[][]mat){
-        for(int column=0;column<mat[0].length;column++){
-            if(mat[r][column]!=0){
-                mat[r][column]=MARKER;
+               if(mat[i][j]==marker){
+                   mat[i][j]=0;
+               }
             }
         }
     }
     
+    private void setRow0(int r,int[][]mat){
+        for(int column=0;column<mat[0].length;column++){
+            if(mat[r][column]!=0){
+                mat[r][column]=marker;
+            }
+        }
+    }
     private void setCol0(int c,int[][]mat){
         for(int row=0;row<mat.length;row++){
             if(mat[row][c]!=0){
-                mat[row][c]=MARKER;
+                mat[row][c]=marker;
             }
         }
     }
