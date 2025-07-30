5bf125681628362b8070e6b05e1f273beb46e5de
Pawan Pandey <pawanpandey.3002@gmail.com>
2025-07-30 10:41:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.java
@@ -0,0 +1,25 @@
+// User function Template for Java
+
+class Solution {
+    public int findMin(int[] arr) {
+        // complete the function here
+        int start=0;
+        int end=arr.length-1;
+        
+        int ans=Integer.MAX_VALUE;
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+            if(arr[start]<=arr[mid]){
+                ans=Math.min(arr[start],ans);
+                start=mid+1;
+            }else{
+                ans=Math.min(arr[mid],ans);
+                end=mid-1;
+            }
+            
+        }
+        return ans;
+    }
+}
