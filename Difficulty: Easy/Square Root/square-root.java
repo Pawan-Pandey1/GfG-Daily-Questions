1109d14fdb27ac0f0a93ff52aba1a4575f51401f
Pawan Pandey <pawanpandey.3002@gmail.com>
2025-07-31 12:48:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.java
@@ -0,0 +1,20 @@
+class Solution {
+    int floorSqrt(int n) {
+        // code here
+        int start=0;
+        int end=n;
+        int ans=-1;
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            
+            if(mid*mid<=n){
+                ans=mid;
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+}
