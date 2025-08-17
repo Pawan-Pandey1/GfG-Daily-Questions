be67580e937d202e6adda2f3e363181bf0b244bf
Pawan Pandey <pawanpandey.3002@gmail.com>
2025-08-17 17:54:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.java
@@ -0,0 +1,69 @@
+/*
+The structure of linked list is the following
+
+class Node
+{
+int data;
+Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    // Function to sort the given linked list using Merge Sort.
+    private static Node findMiddle(Node head){
+        Node slow=head;
+        Node fast=head.next;
+        
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    
+    private static Node mergeLL(Node left, Node right){
+        Node dummy=new Node(-1);
+        Node temp=dummy;
+        
+        while(left!=null && right!=null){
+            if(left.data<right.data){
+                temp.next=left;
+                left=left.next;
+            }else{
+                temp.next=right;
+                right=right.next;
+            }
+            temp=temp.next;
+        }
+        
+       if(left!=null){
+            temp.next=left;
+        }else{
+            temp.next=right;
+        }
+        
+        return dummy.next;
+    }
+    static Node mergeSort(Node head) {
+        // add your code here
+        
+        if(head==null || head.next==null){
+            return head;
+        }
+        
+        Node middleNode=findMiddle(head);
+        Node rightHead = middleNode.next;
+        middleNode.next = null;
+
+        
+        Node left=mergeSort(head);
+        Node right=mergeSort(rightHead);
+        
+        return mergeLL(left,right);
+    }
+}
