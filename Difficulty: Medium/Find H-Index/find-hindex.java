d704de4062769f0692af14ee560f9fe1b57ee857
Pawan Pandey <pawanpandey.3002@gmail.com>
2025-08-18 21:29:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -0,0 +1,28 @@
+// User function Template for Java
+class Solution {
+    // Function to find hIndex
+    public int hIndex(int[] citations) {
+        // code here
+        int n=citations.length;
+        
+        int[]bucket=new int[n+1];
+        
+        for(int i=0;i<n;i++){
+            if(citations[i]>n){
+                bucket[n]++;
+            }else{
+                bucket[citations[i]]++;
+            }
+        }
+        
+        int count=0;
+        for(int i=n;i>=0;i--){
+            count+=bucket[i];
+            
+            if(count>=i){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
