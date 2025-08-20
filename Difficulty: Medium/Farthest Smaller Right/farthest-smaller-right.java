fc9b896ca51d36cf3d474aff6fdede953339be08
Pawan Pandey <pawanpandey.3002@gmail.com>
2025-08-20 12:07:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Farthest Smaller Right/farthest-smaller-right.java
+++ Difficulty: Medium/Farthest Smaller Right/farthest-smaller-right.java
@@ -1,39 +1,40 @@
 class Solution {
-    public static int binS(int[] arr,int[]max, int val,int s){
-        int e=arr.length-1;
-        int ans=-1;
-        while(s<=e){
-            int m=(s+e)/2;
-            
-            if(arr[max[m]]<val){
-                ans=m;
-                s=m+1;
-            }
-            else{
-                e=m-1;
+    private int binaryS(int[] arr, int[] minIndex, int val, int start) {
+        int end = arr.length - 1;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[minIndex[mid]] < val) {
+                ans = mid;
+                start = mid + 1; 
+            } else {
+                end = mid - 1;
             }
         }
         return ans;
-        
     }
+
     public ArrayList<Integer> farMin(int[] arr) {
-        int n=arr.length;
-        int [] max=new int[n];
-        max[n-1]=n-1;
-        for(int i=n-2;i>=0;i--){
-            if(arr[i]<arr[max[i+1]]){
-                max[i]=i;
-            }
-            else{
-                max[i]=max[i+1];
+        int n = arr.length;
+
+        int[] minIndex = new int[n];
+        minIndex[n - 1] = n - 1;
+
+    
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[minIndex[i + 1]]) {
+                minIndex[i] = i;
+            } else {
+                minIndex[i] = minIndex[i + 1];
             }
         }
-        ArrayList<Integer>ans= new ArrayList<>();
-        //int ans[]= new arr[n];
-        for(int i=0;i<n;i++){
-            ans.add(binS(arr,max,arr[i],i));
+
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            result.add(binaryS(arr, minIndex, arr[i], i + 1));
         }
-        return ans;
-        
+        return result;
     }
 }
