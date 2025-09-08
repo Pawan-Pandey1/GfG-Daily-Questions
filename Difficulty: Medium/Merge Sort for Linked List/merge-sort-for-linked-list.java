04366a8001d9800012d2af93c52234b9816bd2b8
Pawan Pandey <pawanpandey.3002@gmail.com>
2025-09-08 22:58:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.java
+++ Difficulty: Medium/Merge Sort for Linked List/merge-sort-for-linked-list.java
@@ -1,12 +1,20 @@
 /*
-The structure of linked list is the following
+class Node {
+    int data;
+    Node next;
 
-class Node
-{
-int data;
-Node next;
-    Node(int key)
-    {
+    Node(int key) {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+/*
+class Node {
+    int data;
+    Node next;
+    Node(int key) {
         data = key;
         next = null;
     }
@@ -15,55 +23,54 @@ Node next;
 
 class Solution {
     // Function to sort the given linked list using Merge Sort.
-    private static Node findMiddle(Node head){
-        Node slow=head;
-        Node fast=head.next;
-        
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+    
+    private Node findMiddle(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
         return slow;
     }
-    
-    private static Node mergeLL(Node left, Node right){
-        Node dummy=new Node(-1);
-        Node temp=dummy;
-        
-        while(left!=null && right!=null){
-            if(left.data<right.data){
-                temp.next=left;
-                left=left.next;
-            }else{
-                temp.next=right;
-                right=right.next;
+
+    private Node mergeLL(Node left, Node right) {
+        Node dummy = new Node(-1);
+        Node temp = dummy;
+
+        while (left != null && right != null) {
+            if (left.data < right.data) {
+                temp.next = left;
+                left = left.next;
+            } else {
+                temp.next = right;
+                right = right.next;
             }
-            temp=temp.next;
+            temp = temp.next;
         }
-        
-       if(left!=null){
-            temp.next=left;
-        }else{
-            temp.next=right;
+
+        if (left != null) {
+            temp.next = left;
+        } else {
+            temp.next = right;
         }
-        
+
         return dummy.next;
     }
-    static Node mergeSort(Node head) {
-        // add your code here
-        
-        if(head==null || head.next==null){
+
+    public Node mergeSort(Node head) {
+        if (head == null || head.next == null) {
             return head;
         }
-        
-        Node middleNode=findMiddle(head);
+
+        Node middleNode = findMiddle(head);
         Node rightHead = middleNode.next;
         middleNode.next = null;
 
-        
-        Node left=mergeSort(head);
-        Node right=mergeSort(rightHead);
-        
-        return mergeLL(left,right);
+        Node left = mergeSort(head);
+        Node right = mergeSort(rightHead);
+
+        return mergeLL(left, right);
     }
 }
